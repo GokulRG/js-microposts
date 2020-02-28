9a0f8ecfd216e6e155274a8b6a8bf16e7012b392
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2020-02-28 04:40:31 -0800

    Modules Practice
    Practising various syntaxes for modules (CommonJS and ES2015) and this commit will be used for future reference to this lesson.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /*jshint esversion: 8 */
-const greeting = 'Hello World';
-console.log(greeting);
+// This is how we import or use a module
+const person = require('./mymodule1'); 
+// This is the same way you can also bring in a node_module. But in case of node modules you just specify the 
+// name and not the path. The default path is the node modules folder. So you can do something like,
+// const express = require('express');
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
+// Import ES2015 module. This is how you import multiple things from a module
+// import { person as es2015person, sayHello, sayGoodbye } from './mymodule2';
 
-getData('https://jsonplaceholder.typicode.com/posts');
+// If you want to import everything from a module
+import * as module2 from './mymodule2';
+
+// How you import a default export
+// import greeting from './mymodule2';  without the braces
+
+console.log(person.name); // prints the name that you have exported from your module.
+console.log(module2.person.age); //prints the age of the person from es2015 module.
+console.log(module2.sayHello());
+console.log(module2.sayGoodbye());
+console.log(module2.default); // If you have a default export, you can access it like so.
+
